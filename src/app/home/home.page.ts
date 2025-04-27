78c59ed684a16b2c2a6b4c38ca1baaaeb555e51d
João Souza <91389107+iamjonesss@users.noreply.github.com>
2025-04-27 18:50:19 -0300

    feat: init project

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  newTask: string = '';
+  tasks: string[] = [];
+
+  constructor() {}
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push(this.newTask.trim());
+      this.newTask = '';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+}
